358887f7f0314a490f23f76bff28958a7826432c
Imawaffle <cchang61@my.centennialcollege.ca>
2021-03-17 19:48:28 -0400

    Added UI handling

---
+++ WhatsPopin/Assets/Scripts/FoodSO.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Create Food")]
+public class FoodSO : ScriptableObject
+{
+    public string iName;
+    public Sprite foodImage;
+}
---
+++ WhatsPopin/Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public List<FoodSO> foodList;
+    public Button recipeSelect;
+
+    private void Start()
+    {
+        recipeSelect.image.sprite = foodList[0].foodImage;
+    }
+}

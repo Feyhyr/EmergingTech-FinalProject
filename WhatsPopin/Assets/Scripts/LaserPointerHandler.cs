71c938c709638a368fa0e856a2d6c65055a39510
Imawaffle <cchang61@my.centennialcollege.ca>
2021-04-07 15:11:36 -0400

    Updated MainMenuScene

---
+++ WhatsPopin/Assets/Scripts/LaserPointerHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Valve.VR.Extras;
+using System.Linq;
+using UnityEngine.UI;
+using Valve.VR.InteractionSystem;
+
+public class LaserPointerHandler : MonoBehaviour
+{
+    private List<SteamVR_LaserPointer> laserPtrRefs;
+
+    private void Awake()
+    {
+        laserPtrRefs = FindObjectsOfType<SteamVR_LaserPointer>().ToList(); //convert array to list
+
+        foreach (SteamVR_LaserPointer x in laserPtrRefs)
+        {
+            x.PointerClick += PointerClickCallback;
+        }
+    }
+
+    public void PointerClickCallback(object sender, PointerEventArgs e)
+    {
+        if (e.target.GetComponent<Button>() != null)
+        {
+            e.target.GetComponent<Button>().onClick.Invoke(); //invoke the button we just clicked
+        }
+    }
+}

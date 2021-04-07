5efc976735a70a2f3d120f76466f1e68b0dae386
Merik <m.ally94@yahoo.ca>
2021-04-07 15:12:14 -0400

    Added Transform script

---
+++ WhatsPopin/Assets/Scripts/FinishedFood.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishedFood : MonoBehaviour
+{
+
+    private GameManager gm;
+
+    public Transform foodPlacement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject gameMger = GameObject.Find("GameManager");
+        gm = gameMger.GetComponent<GameManager>();
+
+        //gm.foodList[index].foodPrefab.transform = foodPlacement;
+
+        Instantiate(gm.foodList[gm.foodIndex].foodPrefab, foodPlacement);
+
+    }
+
+}

640ced33077233c136b8c4f9ddc4dd1155f29851
Merik <m.ally94@yahoo.ca>
2021-04-07 18:29:33 -0400

    Added Volume Slider and Functions

--- WhatsPopin/Assets/Scripts/AudioManager.cs
+++ WhatsPopin/Assets/Scripts/AudioManager.cs
@@ -7,6 +7,11 @@ using UnityEngine.UI;
 public class AudioManager : Singleton<AudioManager>
 {
     public new AudioSource audio;
+
+    public new AudioSource audioTwo;
+
+    private float sfxVolume = 1f;
+    private float mainMenuAudio = 1f;
     //public AudioMixer audioMixer;
 
     public void Play(AudioClip sfxToPlay)
@@ -20,9 +25,27 @@ public class AudioManager : Singleton<AudioManager>
         audio.Play();
     }
 
-   /* public void SetVolume(float volume)
+    public void Update()
     {
-        audioMixer.SetFloat("volume", Mathf.Log10(volume) * 20);
-    }*/
+        audio.volume = sfxVolume;
+        audioTwo.volume = mainMenuAudio;
+    }
+
+    public void updateSFXVolume(float sVolume)
+    {
+        sfxVolume = sVolume;
+    }
+
+    public void updateMenuAUDIO(float mVolume)
+    {
+        mainMenuAudio = mVolume;
+    }
+
+
+
+    /* public void SetVolume(float volume)
+     {
+         audioMixer.SetFloat("volume", Mathf.Log10(volume) * 20);
+     }*/
 
 }
